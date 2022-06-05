e776ffe43477a58a77ea465fb6a2269bbcbe2ba5
PJ <peter.wdj@gmail.com>
2022-06-05 17:42:44 +0100

    Initial commit

---
+++ lib/taylors_authentication.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "taylors_authentication/version"
+
+module TaylorsAuthentication
+  class Error < StandardError; end
+  # Your code goes here...
+end
